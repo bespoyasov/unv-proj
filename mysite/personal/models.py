90342e39c90bd5ddaef1c17c7cd3a7e988e0429c
Александр <forjob159@gmail.com>
2017-03-11 17:04:55 +0500

    add: page model for ref collector

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=200, default='')
+    url = models.CharField(max_length=500)
+    ref = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
