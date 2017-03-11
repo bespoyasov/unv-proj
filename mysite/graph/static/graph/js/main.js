2d8388ad21f1c0c121392db300c7e9ae5a07d904
Александр <forjob159@gmail.com>
2017-03-11 18:34:11 +0500

    add: getting data from client

--- mysite/graph/static/graph/js/main.js
+++ mysite/graph/static/graph/js/main.js
@@ -1,1 +1,23 @@
+// Конфиг
+const config = {
+  baseurl: '/',
+  apiPrefix: 'api/v1/'
+}
 
+
+// АПИ
+const getData = () => {
+  const apiUrl = config.baseurl + config.apiPrefix + 'stats'
+
+  return axios
+    .get(apiUrl)
+    .then(res => { return res.data })
+    .catch(err => { console.error(err) })
+}
+
+
+// Основа
+getData()
+  .then(stats => {
+    console.log(stats)
+  })
