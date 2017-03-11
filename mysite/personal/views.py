62979b5c0f871f519a3198ef31295974178619cc
Александр <forjob159@gmail.com>
2017-03-11 15:44:26 +0500

    add: bootstrap site

---
+++ mysite/personal/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content': ['My email', 'email@me.com'] })
